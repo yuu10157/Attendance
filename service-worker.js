2ec8c9b87b87c543d9f02c78a8f13c97bcc5eced
yuu <yuuu10157@gmail.com>
2025-12-19 09:55:58 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+// simple offline cache for GitHub Pages
+const CACHE_NAME = "work-app-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => cached || fetch(req).catch(() => caches.match("./index.html")))
+  );
+});
